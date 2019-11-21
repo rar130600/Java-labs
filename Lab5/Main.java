37ab654e2886739857a999b6d7780f660801265c
Alexander <rar130600@rar130600>
2019-11-21 20:10:28 +0300

    Lab5: removed PropertiesReader class

--- Lab5/Main.java
+++ Lab5/Main.java
@@ -1,49 +1,24 @@
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Map;
+import java.io.*;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (PrintWriter out = new PrintWriter(new FileWriter("content.properties"))) {
-            Properties properties = new Properties();
-            properties.setProperty("email", "mail@mail.com");
-            properties.setProperty("first_name", "Alex");
-            properties.setProperty("last_name", "Rakhmatullin");
-            properties.store(out, "test properties");
+            Properties propertiesCreate = new Properties();
+            propertiesCreate.setProperty("email", "mail@mail.com");
+            propertiesCreate.setProperty("first_name", "Alex");
+            propertiesCreate.setProperty("last_name", "Rakhmatullin");
+            propertiesCreate.store(out, "test properties");
         } catch (FileNotFoundException except) {
             System.err.println("File is not found: " + except.getMessage());
         } catch (IOException except) {
             except.printStackTrace();
         }
 
-        try {
-            PropertiesReader propertiesReader = new PropertiesReader("content.properties");
-
-            System.out.println("Get properties:");
-            System.out.println("Email: " + propertiesReader.getProperty("email"));
-            System.out.println("First name: " + propertiesReader.getProperty("first_name"));
-            System.out.println("Last name: " + propertiesReader.getProperty("last_name"));
-            System.out.println();
-
-            System.out.println("Output by function:");
-            propertiesReader.list(System.out);
-            System.out.println();
-
-            System.out.println("Output by Map:");
-            Map<String, String> map = propertiesReader.getAsMap();
-            map.forEach((key, value) -> System.out.println("Key: " + key + " Value: " + value));
-            System.out.println();
-
-            System.out.println("Set new property:\n\"Key: login Value: programmer\"");
-            propertiesReader.setProperty("login", "programmer");
-            System.out.println();
-
-            System.out.println("Output properties:");
-            propertiesReader.list(System.out);
-
+        try (BufferedReader br = new BufferedReader(new FileReader("content.properties"))){
+            Properties propertiesLoad = new Properties();
+            propertiesLoad.load(br);
+            propertiesLoad.list(System.out);
         } catch (FileNotFoundException except) {
             System.err.println("File is not found: " + except.getMessage());
         } catch (IOException except) {
